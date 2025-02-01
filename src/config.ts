2b0b546099cd0582e1cf9674ce2d4c4050a0eb2e
mi4u <michal.lipinski@gmail.com>
2025-02-02 00:50:29 +0100

    gogogo

--- src/config.ts
+++ src/config.ts
@@ -15,3 +15,30 @@ export const pr = process.env.pr
 export const includePatterns = process.env.includePatterns
 export const excludePatterns = process.env.excludePatterns
 export const maxLength = process.env.maxLength
+
+
+
+
+
+
+// # Commonly used exit codes
+const ECODE = {
+  SUCCESS: 0,
+  OUTDATED: 1,
+  AUTH_FAILED: 2,
+  SERVER_ERROR: 3,
+  MISSING_BINARY: 5,
+  INVALID_PARAMETER: 6,
+  MISSING_DEPENDENCY: 7,
+  CONDITION_NOT_SATISFIED: 8,
+}
+
+const DEFAULT_OPTIONS = {
+  MODEL: 'deepseek-chat',
+  BASE_URL: 'https://api.deepseek.com',
+  USER_PROMPT: 'Please review the following code changes:',
+  SYS_PROMPT: 'You are a professional code review assistant responsible for analyzing code changes in GitHub Pull Requests. Identify potential issues such as code style violations, logical errors, security vulnerabilities, and provide improvement suggestions. Clearly list the problems and recommendations in a concise manner.',
+}
+
+// # If the PR title or body contains any of these keywords, skip the review
+const IGNORE_REVIEW_KEYWORDS = ['skip review', 'skip cr']
--- src/utils/gh-actions.ts
+++ src/utils/gh-actions.ts
@@ -10,6 +10,41 @@ const {
   data: { login },
 } = await octokit.rest.users.getAuthenticated();
 
+export const getChangedFiles = async (baseRef:string, headRef:string)=>{
+   return await Bun.$`git diff --name-only ${baseRef} ${headRef} `.text()
+}
+
+export const getChanged = async (baseRef:string, headRef:string, file?:string)=>{
+    if(file)
+   return await Bun.$`git diff  ${baseRef} ${headRef} -- ${file}`.text()
+    return await Bun.$`git diff  ${baseRef} ${headRef}`.text()
+}
+
+export const isGitRepo = async ()=>{
+   const result = await Bun.$`git rev-parse --is-inside-work-tree`.text()
+   return result.trim() === 'true'
+}
+
+// git diff  ${baseRef} ${headRef} -- ${file} 
+// git diff --name-only ${base} ${head} 
+
+export const getPrInfo = async ()=>{
+    const result = await Bun.$`gh pr view ${config.pr} --repo ${config.repo} --json title,body,baseRefOid,headRefOid,headRepository,state
+`.json() as {
+    baseRefOid: string
+  body: string
+  headRefOid: string
+  headRepository: {
+    id:string,
+    name:string
+  },
+  state: string
+  title: string
+
+}
+return result
+}
+
 export const getDiff = async (baseUrl: string): Promise<string> => {
   const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
   const [owner, repo] = new URL(baseUrl).pathname.split('/').slice(1, 3);
