6250a4315898a27ed53b8f284a7004cb5c70e7d3
mi4u <michal.lipinski@gmail.com>
2025-02-02 15:11:59 +0100

    jo

--- src/utils/gh-actions.ts
+++ src/utils/gh-actions.ts
@@ -1,6 +1,4 @@
-import { Octokit } from "octokit";
 import * as config from '../config'
-import { ShellError } from "bun";
 import { logger } from "./logger";
 
 
@@ -16,15 +14,15 @@ export const getChangedFiles = async (baseRef: string, headRef: string) => {
   logger.info(await Bun.$`ls `.text())
   logger.warn(`git diff --name-only ${baseRef} ${headRef} `);
   logger.info(`git diff --name-only ${baseRef} ${headRef} `);
-  try{
+  try {
     const files = await Bun.$`git diff --name-only ${baseRef} ${headRef} `.text()
-  return files.split("\n").map(f => f.trim()).filter(f => f.length > 1)
-} catch (err:any ) {
-  logger.warn(err.stdout.toString());
-  logger.warn(err.stderr.toString());
-  logger.error(`Failed with code ${err.exitCode}`);
-  throw err
-}
+    return files.split("\n").map(f => f.trim()).filter(f => f.length > 1)
+  } catch (err: any) {
+    logger.warn(err.stdout.toString());
+    logger.warn(err.stderr.toString());
+    logger.error(`Failed with code ${err.exitCode}`);
+    throw err
+  }
 }
 export const getChangedSummary = async (baseRef: string, headRef: string) => {
   const files = await Bun.$`git diff  --name-status ${baseRef} ${headRef} `.text()
@@ -32,33 +30,31 @@ export const getChangedSummary = async (baseRef: string, headRef: string) => {
 }
 
 export const getFileDiff = async (baseRef: string, headRef: string, file?: string) => {
-  if(file){
+  if (file) {
     logger.log(`git diff  ${baseRef} ${headRef} -- ${file}`)
-  const { stdout, stderr, exitCode } = await Bun.$`git diff  ${baseRef} ${headRef} -- ${file}`
-  if(exitCode!==0){
-    const decoder = new TextDecoder();
+    const { stdout, stderr, exitCode } = await Bun.$`git diff  ${baseRef} ${headRef} -- ${file}`
+    if (exitCode !== 0) {
+      const decoder = new TextDecoder();
 
-    logger.error({
-      error:"is error!!!",
-      msg: decoder.decode(stderr)
-    })
-    return stderr.toString()
+      logger.error({
+        error: "is error!!!",
+        msg: decoder.decode(stderr)
+      })
+      return stderr.toString()
+    }
+    return stdout.toString()
   }
-  return stdout.toString()
-  }
- 
+
   return await Bun.$`git diff  ${baseRef} ${headRef}`.text()
 
 }
 
 export const isGitRepo = async () => {
-  
+
   const result = await Bun.$`git rev-parse --is-inside-work-tree`.text()
   return result.trim() === 'true'
 }
 
-// git diff  ${baseRef} ${headRef} -- ${file} 
-// git diff --name-only ${base} ${head} 
 export const addComment = async (comment: string, file: string, commit: string) => {
 
   return await Bun.$`gh api --method POST -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/${config.repo}/pulls/${config.pr}/comments -f "body=${comment}" -f "commit_id=${commit}" -f "path=${file}"`.text()
--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -1,51 +1,51 @@
 import { OpenAI } from 'openai';
 import { zodResponseFormat } from "openai/helpers/zod";
 import { z } from "zod";
-import {prompts} from './prompts/prompts'
+import { prompts } from './prompts/prompts'
 
 
 
 
 const ReviewSchema = z.object({
-  review:z.union([z.object({
+  review: z.union([z.object({
     is_comment_needed: z.literal(true),
-    is_action_required:z.boolean().describe("should we address this issue?"),
-    category: z.enum(["bug", "idea", "critical-bug","might cause problem", "optimalization","fix","consistency","style"]),
+    is_action_required: z.boolean().describe("should we address this issue?"),
+    category: z.enum(["bug", "idea", "critical-bug", "might cause problem", "optimalization", "fix", "consistency", "style"]),
     comment: z.string().describe("your code comment."),
 
 
   }), z.object({
-    is_comment_needed:z.literal(false).describe('this change is fine and dont need your feedback.')
-  })]).default({is_comment_needed:false}),
+    is_comment_needed: z.literal(false).describe('this change is fine and dont need your feedback.')
+  })]).default({ is_comment_needed: false }),
 
 
-  code_improvement:z.enum(['-5','4','3','2','1','0','1','2','3','4','5']).default('0').describe("in scale from -5 to 5 code improvement after this change. -5 mean bad change, 5 mean great change")
+  code_improvement: z.enum(['-5', '4', '3', '2', '1', '0', '1', '2', '3', '4', '5']).default('0').describe("in scale from -5 to 5 code improvement after this change. -5 mean bad change, 5 mean great change")
 })
 
 
 
-const codeBlockSeparator='```'
-const system_prompt=prompts.tsdev
+const codeBlockSeparator = '```'
+const system_prompt = prompts.tsdev
 
-export const reviewChanges = async (diff:string,baseURL:string,apiKey:string,modelName: string, prompt=system_prompt,extraInfo?:string) => {
-  const openai = new OpenAI({ apiKey,  baseURL});
-    const formatedDiff=["",codeBlockSeparator,"",diff,"",codeBlockSeparator].join("\n")
-    // console.log('system:',[prompt,extraInfo||""].join('\n') )
-    const response = await openai.beta.chat.completions.parse({
-      model: modelName,
-      messages: [
-        { role: 'system', content:[prompt,extraInfo||""].join('\n') },
-        { role: 'user', content: `Review the following changes: ${formatedDiff}` },
-      ],
-      // max_completion_tokens: 128,
-      response_format: zodResponseFormat(ReviewSchema, "review"),
-      temperature:0, 
+export const reviewChanges = async (diff: string, baseURL: string, apiKey: string, modelName: string, prompt = system_prompt, extraInfo?: string) => {
+  const openai = new OpenAI({ apiKey, baseURL });
+  const formatedDiff = ["", codeBlockSeparator, "", diff, "", codeBlockSeparator].join("\n")
+  // console.log('system:',[prompt,extraInfo||""].join('\n') )
+  const response = await openai.beta.chat.completions.parse({
+    model: modelName,
+    messages: [
+      { role: 'system', content: [prompt, extraInfo || ""].join('\n') },
+      { role: 'user', content: `Review the following changes: ${formatedDiff}` },
+    ],
+    // max_completion_tokens: 128,
+    response_format: zodResponseFormat(ReviewSchema, "review"),
+    temperature: 0,
 
-    },{
-      stream:false, maxRetries:5
-    });
+  }, {
+    stream: false, maxRetries: 5
+  });
 
-    return response.choices[0].message.parsed;
+  return response.choices[0].message.parsed;
 
   // return response;
 };
