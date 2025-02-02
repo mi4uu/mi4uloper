01a9c5125670571f1d425e0bf5544989670f9c76
mi4u <michal.lipinski@gmail.com>
2025-02-02 08:36:36 +0100

    dd

--- src/utils/gh-actions.ts
+++ src/utils/gh-actions.ts
@@ -1,5 +1,6 @@
 import { Octokit } from "octokit";
 import * as config from '../config'
+import { ShellError } from "bun";
 
 
 
@@ -16,7 +17,14 @@ export const getChangedFiles = async (baseRef: string, headRef: string) => {
 
 
   const files = await Bun.$`git diff --name-only ${baseRef} ${headRef} `.text()
+  try{
   return files.split("\n").map(f => f.trim()).filter(f => f.length > 1)
+} catch (err:any ) {
+  console.log(`Failed with code ${err.exitCode}`);
+  console.log(err.stdout.toString());
+  console.log(err.stderr.toString());
+  throw err
+}
 }
 export const getChangedSummary = async (baseRef: string, headRef: string) => {
   const files = await Bun.$`git diff  --name-status ${baseRef} ${headRef} `.text()
@@ -24,9 +32,16 @@ export const getChangedSummary = async (baseRef: string, headRef: string) => {
 }
 
 export const getFileDiff = async (baseRef: string, headRef: string, file?: string) => {
+  try{
   if (file)
     return await Bun.$`git diff  ${baseRef} ${headRef} -- ${file}`.text()
   return await Bun.$`git diff  ${baseRef} ${headRef}`.text()
+} catch (err:any ) {
+  console.log(`Failed with code ${err.exitCode}`);
+  console.log(err.stdout.toString());
+  console.log(err.stderr.toString());
+  throw err
+}
 }
 
 export const isGitRepo = async () => {
